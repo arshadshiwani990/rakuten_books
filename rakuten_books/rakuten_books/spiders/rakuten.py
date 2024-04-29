3ef4a3e10e088b2f869728d24cf0907f85e2486d
muhammad arshad <arshadshiwani990@gmail.com>
2024-04-29 11:41:48 +0500

    created spider

---
+++ rakuten_books/rakuten_books/spiders/rakuten.py
@@ -0,0 +1,50 @@
+import scrapy
+import os
+
+class RakutenSpider(scrapy.Spider):
+    
+    custom_settings = {
+		"FEEDS": {
+			os.path.join("scrapy_output", "rakuten.csv"): {
+				"format": "csv",
+				"overwrite": True,
+			}
+		},
+		"ROBOTSTXT_OBEY": False,
+		
+	}
+	
+ 
+ 
+    name = 'rakuten'
+
+    start_urls = ['https://books.rakuten.co.jp/search?g=001&e=1&o=0&l-id=search-c-pager']
+
+    def parse(self, response):
+        
+        
+        divs=response.xpath('//div[@class="rbcomp__item-list__item"]')
+      
+        for div in divs:
+            isbn=div.xpath('.//p[@class="gbs"]')
+            if isbn:
+                isbn=isbn.xpath('.//@id').get().replace('ISBN','')
+                print(isbn)     
+                
+                price=div.xpath('.//p[@class="rbcomp__price"]//em//text()').get().replace('円','')
+                print(price) 
+
+                
+                item={}
+                item['isbn']=isbn
+                item['price']=price
+                
+                yield item
+                
+                print('------------------------')
+
+        
+        nextpage=response.xpath('//ul[@class="rbcomp__pager"]/following-sibling::div/a/@href').get()
+        if nextpage:
+            print(nextpage)
+            yield scrapy.Request(url=nextpage, callback=self.parse)
